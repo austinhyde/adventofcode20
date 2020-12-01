aee76ad1fb23d10acd4be63d31c8469987bd82b0
Austin Hyde <austin109@gmail.com>
2020-12-01 18:53:37 -0500

    day 1 part 2 solved

--- day1-expense-report/src/main.rs
+++ day1-expense-report/src/main.rs
@@ -1,28 +1,46 @@
-/**
-the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
-
-Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
-*/
-
 fn main() {
     let input = include_str!("input.txt");
 
-    println!("Answer: {:?}", expense_report(input));
+    /* Part 1
+        The Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+        Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+    */
+    println!("Part 1 Answer: {:?}", expense_report(input, 2));
     // correct answer: 539851
+
+    /* Part 2
+        find three numbers in your expense report that meet the same criteria
+    */
+    println!("Part 2 Answer: {:?}", expense_report(input, 3));
+    // correct answer: 212481360
 }
 
-fn expense_report(input: &str) -> Result<i32, String> {
-    /*
-    we need to find two entries that sum to 2020.
-    the naive solution is to compare every entry to every other entry.
+fn expense_report(input: &str, n_candidates: usize) -> Option<i32> {
+    /* part 1:
+        we need to find two entries that sum to 2020.
+        the naive solution is to compare every entry to every other entry.
 
-    we can do better though:
-    if we sort the input, we only need to check half the inputs
-    against the other half, and we can use a binary search to
-    rapidly find candidates
+        we can do better though:
+        if we sort the input, we only need to check half the inputs
+        against the other half, and we can use a binary search to
+        rapidly find candidates
 
-    the naive solution is, worst case, O(n^2)
-    this solution is O(n log n) for the sort and O(n log n) for the search
+        the naive solution is, worst case, O(n^2)
+        this solution is O(n log n) for the sort and O(n log n) for the search (O(n log n) total)
+
+       part 2:
+        we need to find three (or generally C) entries that sum to 2020
+
+        observing that expense_report(entries, target, C=1) is just entries[entries.binary_search(target)],
+        and expense_report(entries, target, C=2) is entries[i] * entries[entries[i+1..].binary_search(target-entries[i])]
+        then we can generalize to
+
+            expense_report(entries, target, C > 1) = entries[i] * expense_report(entries[i+1..], target-entries[i], C-1)
+            expense_report(entries, target, 1) = entries[entries.binary_search(target)]
+        
+        this gives us O(n^(C-1) log n) performance, over the naive O(n^C)
+
+        I'm sure there's more clever ways to do this, but I'm not sure how that would work, so we'll go with this implementation
     */
 
     // first, let's parse and sort our input
@@ -31,31 +49,47 @@ fn expense_report(input: &str) -> Result<i32, String> {
         .collect::<Vec<_>>();
     entries.sort_unstable();
 
-    // for every element, binary search the rest of the list
+    expense_report_impl(&entries, 2020, n_candidates)
+}
+
+fn expense_report_impl(entries: &[i32], target: i32, candidates: usize) -> Option<i32> {
+    if candidates == 1 {
+        if let Ok(idx) = entries.binary_search(&target) {
+            return Some(entries[idx])
+        }
+        return None
+    }
+
+    // for every element, recurse on the rest of the list
     for (i, first) in entries.iter().enumerate() {
-        let target = 2020 - first;
-        let rest = &entries[i+1..];
-        if let Ok(second_idx) = rest.binary_search(&target) {
-            return Ok(first * rest[second_idx])
+        if let Some(second) = expense_report_impl(&entries[i+1..], target - first, candidates-1) {
+            return Some(first * second);
         }
     }
-    Err("No value found".to_string())
+    None
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn example1() {
-        let input = "\
+    const EXAMPLE_INPUT: &str = "\
             1721\n\
             979\n\
             366\n\
             299\n\
             675\n\
             1456\n";
-        let answer = expense_report(input).unwrap();
+
+    #[test]
+    fn part1_example1() {
+        let answer = expense_report(EXAMPLE_INPUT, 2).unwrap();
         assert_eq!(answer, 514579);
+    }
+
+    #[test]
+    fn part2_example1() {
+        let answer = expense_report(EXAMPLE_INPUT, 3).unwrap();
+        assert_eq!(answer, 241861950);
     }
 }
