7167e0ec649ba4fa9ed38cae2d11c3011fd1c9c4
Austin Hyde <austin109@gmail.com>
2020-12-02 08:47:47 -0500

    day 1 part 2 golf

---
+++ day1-expense-report-golf/part2.js
@@ -0,0 +1,4 @@
+d=require('fs').readFileSync(0,'utf8').split`
+`;console.log(d.find(i=>j=d.find(j=>k=d.find(k=>2020-i-j-k==0)))*j*k)
+// f=require('fs').readFileSync(0,'utf8').split`
+// `::find;console.log(f(i=>j=f(j=>k=f(k=>-(-i-j-k)==2020)))*j*k)
