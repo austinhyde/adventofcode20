3383bab50da4ce80fe57d2a9b422b579301c7220
Austin Hyde <austin109@gmail.com>
2020-12-06 12:20:39 -0500

    day 6 part 2 solved

--- day6-custom-customs/src/main.rs
+++ day6-custom-customs/src/main.rs
@@ -1,10 +1,11 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = include_str!("input.txt");
     println!("Part 1 Answer: {}", part1(input));
     // Correct answer: 6885
     println!("Part 2 Answer: {}", part2(input));
+    // Correct answer: 3550
 }
 
 /** Part 1:
@@ -25,7 +26,22 @@ fn part1(input: &str) -> usize {
 
 */
 fn part2(input: &str) -> usize {
-    0
+    input.split("\n\n")
+        .map(|group| {
+            let n_people = group.lines().count();
+
+            group.chars()
+                .filter(|c| !c.is_whitespace())
+                .fold(HashMap::new(), |map, ans| {
+                    let mut map = map;
+                    *map.entry(ans).or_insert(0) += 1;
+                    map
+                })
+                .into_iter()
+                .filter(|(_q, n_ans)| *n_ans == n_people)
+                .count()
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -42,7 +58,7 @@ mod tests {
     #[test]
     fn part2_example1() {
         let input = include_str!("example1.txt");
-        let expected = 0;
+        let expected = 6;
         assert_eq!(expected, part2(input));
     }
 }
